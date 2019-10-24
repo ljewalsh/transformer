44cbc65f990d70baf5e1cacf80fc0cb4dd209498
Lucy-Jane Walsh <ljewalsh@gmail.com>
2019-10-24 14:43:31 +1300

    Update hashtag_extract test to include examples of hashtags with a number inside AND hashtags with capital letters

--- transformer/transforms/string/hashtag_extract_test.py
+++ transformer/transforms/string/hashtag_extract_test.py
@@ -8,11 +8,15 @@ class TestStringHashtagExtractTransform(unittest.TestCase):
         self.assertEqual(transformer.transform(None), [])
         self.assertEqual(transformer.transform(u'I have no hashtags'), [])
         self.assertEqual(transformer.transform(u'I have a hashtag #hashtag'), [u'hashtag'])
+        self.assertEqual(transformer.transform(u'My #HASHTAG is capitalised'), [u'HASHTAG'])
+        self.assertEqual(transformer.transform(u'My #ha5htag includes a number'), [u'ha5htag'])
         self.assertEqual(transformer.transform(u'I have two hashtags #hashtag #anotherhashtag'), [u'hashtag', u'anotherhashtag'])
         self.assertEqual(transformer.transform(u'#myhashtag is at the start of my string'), ['myhashtag'])
 
-        # Digits and punctuation should not be captured
-        self.assertEqual(transformer.transform(u'#200'), [])
+        # Hashtags with digits at the start should not be captured
+        self.assertEqual(transformer.transform(u'#2times'), [])
+
+        # Hastags with punctuation should not be captured
         self.assertEqual(transformer.transform(u'#.'), [])
         self.assertEqual(transformer.transform(u'#,'), [])
         self.assertEqual(transformer.transform(u'#,,'), [])
